c9cdfb1c6b7faa6c1dbfafcc0f9dc855a53431a3
awanzada786616 <wasicenter120@gmail.com>
2025-12-29 17:43:42 +0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+// api/proxy.js
+const axios = require('axios');
 
-export default async function handler(req, res) {
-    // AAPKA 5SIM TOKEN INTEGRATED
+module.exports = async (req, res) => {
     const API_TOKEN = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3OTg1NDc3NDAsImlhdCI6MTc2NzAxMTc0MCwicmF5IjoiOTQ2YTQ5OWViOGRmMzYwMTgzNzA4YjlkN2JiMWU4ODkiLCJzdWIiOjM0MDkzMDB9.h_SIUTYe07TtNcYf7etMB-o4I7QsMsiq8990QOQIrMobxdd5flUbGuZIPIUfQsXx_05LjWtLodguTdgixthI9hwN9PDeDDqVtgd9vxYUpERpqU_f0B2gIZJAQcYy9zXcEyK0eACjhGCTIwK-UkpmfV-Kt_KmJ1B_oewy6oODs5uOCqiWtL0n1HhF9yfYKn1liBf23jFmSKyu9UeGvsbfUyFX2_BxsngpUdVFy59bDJI8jWEPWX0LRDvIjcdrr20qe0hxP_hefBYool4HV7XykYLXXagqV-r-ROEcdYCpYK-x0YBt6H1CLVMJ_4Cr286HxiD6a0x-TSaSjFr213coYQ";
     
     const { action, country, service, id } = req.query;
@@ -21,6 +21,6 @@ export default async function handler(req, res) {
         const response = await axios.get(url, { headers });
         res.status(200).json(response.data);
     } catch (error) {
-        res.status(500).json({ error: error.message, details: error.response?.data });
+        res.status(500).json({ error: error.message });
     }
-}
+};
